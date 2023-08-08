9b3f19ab4d72ff7ae1f62fde60476765448f9898
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-08 18:26:22 +0530

    Added : UC1

--- src/com/bl/Generics.java
+++ src/com/bl/Generics.java
@@ -1,6 +1,48 @@
 package com.bl;
 
-public class Generics {
+public class Generics <T extends Comparable<T>> {
 
+    T x, y, z;
+
+    public Generics(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+    }
+
+    public T maximum() {
+
+        return Generics.maximum(x, y, z);
+    }
+
+
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+        T max = x;
+        if (y.compareTo(max) > 0) {
+
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+
+        printMax(x, y, z, max);
+
+        return max; // returns the largest object
+
+
+    }
+
+    private static <T extends Comparable<T>> void printMax(T x, T y, T z, T max) {
+        System.out.printf("Max of %s, %s and %s is %s\n", x, y, z, max);
+    }
+
+
+    public static void main(String[] args) {
+
+        Integer xInt = 13, yInt = 24, zInt = 55;
+        new Generics<>(xInt, yInt, zInt).maximum();
+    }
 
 }
